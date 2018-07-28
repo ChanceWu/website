0b46c5e655d0a9acec4ad085baa1420e3e991edf
Chance <1565222098@qq.com>
2018-07-28 20:11:15 +0800

    利用session保存登录用户信息

--- app.js
+++ app.js
@@ -1,27 +1,43 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
 var path = require('path');
 var mongoose = require('mongoose');
+var mongoStore = require('connect-mongo')(expressSession);
 var Movie = require('./models/movie');
+var User = require('./models/user');
 var _ = require('underscore');
 var port = process.env.PORT || 3003;
 var app = express();
+var dbUrl = 'mongodb://localhost/imooc';
 
-mongoose.connect('mongodb://localhost/imooc')
+mongoose.connect(dbUrl);
 
-app.set('views','./views/pages')
-app.set('view engine','jade')
-app.use(bodyParser())
-app.use(express.static(path.join(__dirname, 'public')))
-app.locals.moment = require('moment')
-app.listen(port)
+app.set('views','./views/pages');
+app.set('view engine','jade');
+app.use(bodyParser());
+app.use(cookieParser());
+app.use(expressSession({
+	secret: 'imooc',
+	store: new mongoStore({
+		url: dbUrl,
+		collection: 'sessions'
+	})
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.locals.moment = require('moment');
+app.listen(port);
 
-console.log('website started on port '+port)
+console.log('website started on port '+port);
 
+// index page
 app.get('/', function(req, res) {
+	console.log('user in session');
+	console.log(req.session.user);
 	Movie.fetch(function(err, movies) {
 		if (err) {
-			console.log(err)
+			console.log(err);
 		}
 		res.render('index', {
 			title: 'imooc 首页',
@@ -29,6 +45,74 @@ app.get('/', function(req, res) {
 		})
 	})
 })
+
+//signup
+app.post('/user/signup', function(req, res){
+	var _user = req.body.user;
+	// 判断创建用户是否重复
+	User.findOne({name: _user.name}, function(err, user){
+		if(err){
+			console.log(err);
+		}
+
+		if(user){
+			return res.redirect('/');
+		}else{
+			var user = new User(_user);
+
+			user.save(function(err, user){
+				if(err){
+					console.log(err);
+				}
+				res.redirect('/admin/userlist');
+			})
+		}
+	})
+})
+
+//signin
+app.post('/user/signin', function(req, res) {
+	var _user = req.body.user;
+	var name = _user.name;
+	var password = _user.password;
+
+	User.findOne({name: name}, function(err, user) {
+		if (err) {
+			console.log(err);
+		}
+		if (!user) {
+			return res.redirect('/');
+		}
+
+		user.comparePassword(password, function(err, isMatch) {
+			if (err) {
+				console.log(err);
+			}
+			if (isMatch) {
+				req.session.user = user;
+				console.log('password is matched');
+				return res.redirect('/');
+			} else {
+				console.log('password is not matched');
+			}
+		})
+	})
+})
+
+// userlist page
+app.get('/admin/userlist', function(req, res) {
+	User.fetch(function(err, users) {
+		if (err) {
+			console.log(err);
+		}
+
+		res.render('userlist', {
+			title: 'imooc 用户列表页',
+			users: users
+		})
+	})
+})
+
 app.get('/movie/:id', function(req, res) {
 	var id = req.params.id;
 	Movie.findById(id, function(err, movie) {
@@ -59,7 +143,7 @@ app.get('/admin/update/:id', function(req, res) {
 	if (id) {
 		Movie.findById(id, function(err, movie) {
 			if (err) {
-				console.log(err)
+				console.log(err);
 			}
 			res.render('admin', {
 				title: 'imooc 后台更新页',
@@ -77,16 +161,16 @@ app.post('/admin/movie/new', function(req, res) {
 	if (id !== 'undefined') {
 		Movie.findById(id, function(err, movie) {
 			if (err) {
-				console.log(err)
+				console.log(err);
 			}
 
 			_movie = _.extend(movie, movieObj)
 			_movie.save(function(err, movie) {
 				if (err) {
-					console.log(err)
+					console.log(err);
 				}
 
-				res.redirect('/movie/' + movie._id)
+				res.redirect('/movie/' + movie._id);
 			})
 		})
 	} else {
@@ -103,10 +187,10 @@ app.post('/admin/movie/new', function(req, res) {
 
 		_movie.save(function(err, movie) {
 			if (err) {
-				console.log(err)
+				console.log(err);
 			}
 
-			res.redirect('/movie/' + movie._id)
+			res.redirect('/movie/' + movie._id);
 		})
 	}
 })
@@ -114,7 +198,7 @@ app.post('/admin/movie/new', function(req, res) {
 app.get('/admin/list', function(req, res) {
 	Movie.fetch(function(err, movies) {
 		if (err) {
-			console.log(err)
+			console.log(err);
 		}
 
 		res.render('list', {
@@ -123,6 +207,7 @@ app.get('/admin/list', function(req, res) {
 		})
 	})
 })
+
 
 // list delete movie
 app.delete('/admin/list', function(req, res) {
@@ -133,85 +218,9 @@ app.delete('/admin/list', function(req, res) {
 			if (err) {
 				console.log(err);
 			} else {
-				res.json({success: 1})
+				res.json({success: 1});
 			}
 		})
 	}
 })
 
-/***
-app.get('/',function(req, res){
-	res.render('index',{
-		title: '首页',
-		movies: [{
-			title: '机械战警1',
-			_id: 1,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警2',
-			_id: 2,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警3',
-			_id: 3,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警4',
-			_id: 4,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警5',
-			_id: 5,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		},{
-			title: '机械战警6',
-			_id: 6,
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
-		}]
-	})
-})
-app.get('/movie/:id',function(req, res){
-	res.render('detail',{
-		title: '详情页',
-		movie: {
-			doctor: '何塞帕迪利亚',
-			country: '美国',
-			title: '机械战警',
-			year: '2014',
-			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
-			language: '英语',
-			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
-			summary: '十多块上课说道好就开始你 和考生等级考试结合看山顶上几十块的数据库倒计时的看法和你上课交水电费看数据库电脑卡时间飞逝'
-		}
-	})
-})
-app.get('/admin/movie',function(req, res){
-	res.render('admin',{
-		title: '后台录入页',
-		movie: {
-			title: '',
-			doctor: '',
-			country: '',
-			year: '',
-			poster: '',
-			flash: '',
-			summary: '',
-			language: ''
-		}
-	})
-})
-app.get('/admin/list',function(req, res){
-	res.render('list',{
-		title: '列表页',
-		movies: [{
-			title: '机械战警',
-			_id: 1,
-			doctor: '何塞帕迪利亚',
-			country: '美国',
-			year: '2014',
-			language: '英语',
-			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf'
-		}]
-	})
-})
-***/
