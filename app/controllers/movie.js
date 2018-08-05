a7a0323ba4addf9fd86ede7164028f0bb663f5a9
Chance <1565222098@qq.com>
2018-08-05 13:38:48 +0800

    实现评论功能，数据的存储查询展示，用户间相互回复功能，对评论的登录限制

--- app/controllers/movie.js
+++ app/controllers/movie.js
@@ -1,14 +1,25 @@
 var Movie = require('../models/movie');
+var Comment = require('../models/comment');
 var _ = require('underscore');
 
 // detail page
 exports.detail = function(req, res) {
 	var id = req.params.id;
 	Movie.findById(id, function(err, movie) {
-		res.render('detail', {
-			title: 'imooc ' + movie.title,
-			movie: movie
-		})
+		Comment
+			.find({movie: id})
+			.populate('from', 'name')
+			.populate('reply.from reply.to', 'name')
+			.exec(function(err, comments) {
+				console.log('comments');
+				console.log(comments);
+				res.render('detail', {
+					title: 'imooc 详情页',
+					movie: movie,
+					comments: comments
+				})
+			})
+		
 	})
 }
 
