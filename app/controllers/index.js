138de81e6a02932026a0455d0553b2d2c7ed2a57
Chance <1565222098@qq.com>
2018-08-06 12:55:15 +0800

    实现分类列表分页

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,3 +16,36 @@ exports.index = function(req, res) {
 			});
 		});
 }
+
+//search page
+exports.search = function(req, res) {
+	var catId = req.query.cat;
+	var page = parseInt(req.query.p);
+	var count = 2;
+	var index = page * count;
+
+	Category
+		.find({_id: catId})
+		.populate({
+			path: 'movies',
+			select: 'title poster'
+		})
+		.exec(function(err, categories){
+			if (err) {
+				console.log(err);
+			}
+
+			var category = categories[0] || {};
+			var movies = category.movies || [];
+			var results = movies.slice(index, index+count);
+
+			res.render('results', {
+				title: 'imooc 结果列表页面',
+				keyword: category.name,
+				currentPage: (page + 1),
+				query: 'cat=' + catId,
+				totalPage: Math.ceil(movies.length / count),
+				movies: results
+			});
+		});
+}
