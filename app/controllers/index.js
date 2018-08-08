075618f5b0b7eab2e30d0d915d7492fdfff60df2
Chance <1565222098@qq.com>
2018-08-08 16:03:24 +0800

    实现搜索功能

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -20,32 +20,54 @@ exports.index = function(req, res) {
 //search page
 exports.search = function(req, res) {
 	var catId = req.query.cat;
-	var page = parseInt(req.query.p);
+	var q = req.query.q;
+	var page = parseInt(req.query.p) || 0;
 	var count = 2;
 	var index = page * count;
 
-	Category
-		.find({_id: catId})
-		.populate({
-			path: 'movies',
-			select: 'title poster'
-		})
-		.exec(function(err, categories){
-			if (err) {
-				console.log(err);
-			}
+	if(catId){
+		Category
+			.find({_id: catId})
+			.populate({
+				path: 'movies',
+				select: 'title poster'
+			})
+			.exec(function(err, categories){
+				if (err) {
+					console.log(err);
+				}
 
-			var category = categories[0] || {};
-			var movies = category.movies || [];
-			var results = movies.slice(index, index+count);
+				var category = categories[0] || {};
+				var movies = category.movies || [];
+				var results = movies.slice(index, index+count);
 
-			res.render('results', {
-				title: 'imooc 结果列表页面',
-				keyword: category.name,
-				currentPage: (page + 1),
-				query: 'cat=' + catId,
-				totalPage: Math.ceil(movies.length / count),
-				movies: results
+				res.render('results', {
+					title: 'imooc 结果列表页面',
+					keyword: category.name,
+					currentPage: (page + 1),
+					query: 'cat=' + q,
+					totalPage: Math.ceil(movies.length / count),
+					movies: results
+				});
 			});
-		});
+	} else {
+		Movie
+			.find({title: new RegExp(q+'.*', 'i')})
+			.exec(function(err, movies){
+				if(err){
+					console.log(err);
+				}
+
+				var results = movies.slice(index, index+count);
+				res.render('results', {
+					title: 'imooc 结果列表页面',
+					keyword: q,
+					currentPage: (page + 1),
+					query: 'q=' + catId,
+					totalPage: Math.ceil(movies.length / count),
+					movies: results
+				})
+			})
+	}
+	
 }
